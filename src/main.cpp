2bad13786e8139afaa33ec4c42ef20510ce9b13b
Oskar-Pietrzko <os.pietrzko@gmail.com>
2024-09-12 20:15:44 +0200

    Updated project, added more checks

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 auto main() -> int {
-    float a, b, c;
+    double a, b, c;
 
     std::cout << "ax^2 + bx^2 + c = 0" << std::endl << std::endl;
 
@@ -15,21 +15,31 @@ auto main() -> int {
     std::cout << "Enter the third number (c): ";
     std::cin >> c;
 
-    const float delta = b * b - 4 * a * c;
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
+                std::cout << "The equation has infinitely many solutions." << std::endl;
+            } else {
+                std::cout << "The equation is inconsistent, no solutions." << std::endl;
+            }
+        } else {
+            const double x = -c / b;
 
-    std::cout << std::endl << "The solution is:" << std::endl;
-
-    if (delta == 0) {
-        const float x1 = -b / (2 * a);
-
-        std::cout << "Exists one solution: x1 = " << x1 << std::endl;
-    } else if (delta > 0) {
-        const float x1 = (-b - std::sqrt(delta)) / (2 * a);
-        const float x2 = (-b + std::sqrt(delta)) / (2 * a);
-
-        std::cout << "Exists two solutions: x1 = " << x1 << ", x2 = " << x2 << std::endl;
+            std::cout << "Linear equation, solution: " << x << std::endl;
+        }
     } else {
-        std::cout << "Solution does not exist." << std::endl;
+        if (const double delta = b * b - 4 * a * c; delta == 0) {
+            const double x1 = -b / (2 * a);
+
+            std::cout << "The quadratic equation has one solution: " << x1 << std::endl;
+        } else if (delta > 0) {
+            const double x1 = (-b - std::sqrt(delta)) / (2 * a);
+            const double x2 = (-b + std::sqrt(delta)) / (2 * a);
+
+            std::cout << "The quadratic equation has two solutions: " << x1 << " and " << x2 << std::endl;
+        } else {
+            std::cout << "The quadratic equation has no solutions." << std::endl;
+        }
     }
 
     std::cin.ignore();
